4f5f521e7a2576ecf0088eeb67fcf5f85e1c1e0c
Angel Castillo <portillocastilloa@gmail.com>
2020-11-12 20:13:09 -0600

    Actualizacion de la rutas de login

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const { Router } = express;
 const router = Router();
 
-
+router.post('/', (req, res) => {
+    console.log(req)
+});
 
 
 module.exports = router;
